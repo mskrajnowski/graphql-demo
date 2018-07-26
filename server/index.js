93ccc847ee82554c96a717cb5567fc6e63b30bf8
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2018-07-27 00:38:32 +0200

    Naive resolvers

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const { ApolloServer, gql } = require("apollo-server")
 const fs = require("fs")
 const path = require("path")
 
+const { Person, Post, PostLike } = require("./db")
+
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = fs.readFileSync(
@@ -12,7 +14,37 @@ const typeDefs = fs.readFileSync(
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
-    Query: {},
+    Query: {
+        person: (_, { id }) => Person.query().findById(id),
+        people: () =>
+            Person.query()
+                .limit(10)
+                .select(),
+        post: (_, { id }) => Post.query().findById(id),
+        posts: () =>
+            Post.query()
+                .limit(10)
+                .select(),
+    },
+    Person: {
+        createdAt: async person => new Date(person.createdAt).toISOString(),
+        updatedAt: async person => new Date(person.updatedAt).toISOString(),
+        posts: async person => person.$relatedQuery("posts"),
+        likes: async person => person.$relatedQuery("likes"),
+        likedPosts: async person => person.$relatedQuery("likedPosts"),
+    },
+    Post: {
+        createdAt: async person => new Date(person.createdAt).toISOString(),
+        updatedAt: async person => new Date(person.updatedAt).toISOString(),
+        author: async post => post.$relatedQuery("author"),
+        likes: async post => post.$relatedQuery("likes"),
+        likedBy: async post => post.$relatedQuery("likedBy"),
+    },
+    Like: {
+        createdAt: async person => new Date(person.createdAt).toISOString(),
+        person: async like => like.$relatedQuery("person").first(),
+        post: async like => like.$relatedQuery("post").first(),
+    },
 }
 
 // In the most basic sense, the ApolloServer can be started
