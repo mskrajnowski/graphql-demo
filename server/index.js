6bf0c659f3a5337e7fac0dd43caecf4c81e259d1
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2018-07-27 09:30:57 +0200

    byId loaders

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server")
 const fs = require("fs")
 const path = require("path")
+const Dataloader = require("dataloader")
+const { keyBy } = require("lodash")
 
 const { Person, Post, PostLike } = require("./db")
-
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = fs.readFileSync(
@@ -15,13 +16,13 @@ const typeDefs = fs.readFileSync(
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
     Query: {
-        person: (_, { id }) => Person.query().findById(id),
+        person: (_, { id }, { loaders }) => loaders.person.byId.load(id),
         people: () =>
             Person.query()
                 .limit(10)
                 .select(),
 
-        post: (_, { id }) => Post.query().findById(id),
+        post: (_, { id }, { loaders }) => loaders.post.byId.load(id),
         posts: () =>
             Post.query()
                 .limit(10)
@@ -39,16 +40,40 @@ const resolvers = {
         createdAt: async person => new Date(person.createdAt).toISOString(),
         updatedAt: async person => new Date(person.updatedAt).toISOString(),
 
-        author: async post => post.$relatedQuery("author"),
+        author: async (post, _, { loaders }) =>
+            loaders.person.byId.load(post.authorId),
         likes: async post => post.$relatedQuery("likes"),
         likedBy: async post => post.$relatedQuery("likedBy"),
     },
     Like: {
         createdAt: async person => new Date(person.createdAt).toISOString(),
 
-        person: async like => like.$relatedQuery("person").first(),
-        post: async like => like.$relatedQuery("post").first(),
+        person: async (like, _, { loaders }) =>
+            loaders.person.byId.load(like.personId),
+        post: async (like, _, { loaders }) =>
+            loaders.post.byId.load(like.postId),
     },
+}
+
+async function context() {
+    const loaders = {
+        person: {
+            byId: new Dataloader(async ids => {
+                const people = await Person.query().findByIds(ids)
+                const peopleById = keyBy(people, "id")
+                return ids.map(id => peopleById[id])
+            }),
+        },
+        post: {
+            byId: new Dataloader(async ids => {
+                const posts = await Post.query().findByIds(ids)
+                const postsById = keyBy(posts, "id")
+                return ids.map(id => postsById[id])
+            }),
+        },
+    }
+
+    return { loaders }
 }
 
 // In the most basic sense, the ApolloServer can be started
@@ -57,6 +82,7 @@ const resolvers = {
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context,
 })
 
 // This `listen` method launches a web-server.  Existing apps
